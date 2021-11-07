b161ebb712a053e1e7db0d7ef7196c2305eb11c5
George Margaritis <margaeor@gmail.com>
2021-11-06 22:11:17 -0400

    Add automatic dataset download

--- create_data.py
+++ create_data.py
@@ -1,14 +1,36 @@
 from collections import OrderedDict
 import pandas as pd
 import os
-
+import zipfile
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+import requests
 
 DATA_PATH = 'data'
 DATASET_PATH = os.path.join(DATA_PATH, 'dataset')
 METADATA_PATH = os.path.join(DATA_PATH, 'metadata')
 OUTPUT_PATH = os.path.join(DATA_PATH, 'output')
+DATASET_NAME = 'psam_pusa.csv'
+
+
+def download_and_extract_zip(url, output):
+    with requests.get(url) as file:
+        with ZipFile(BytesIO(file.content)) as zfile:
+            zfile.extractall(output)
+
 
 if __name__ == '__main__':
+
+    dataset = os.path.join(DATASET_PATH, DATASET_NAME)
+
+    # If dataset doesn't exist, download it and extract it
+    if not os.path.exists(dataset):
+        print("Downloading dataset")
+        download_and_extract_zip(
+            "https://www2.census.gov/programs-surveys/acs/data/pums/2019/1-Year/csv_pus.zip",
+            DATASET_PATH
+        )
 
     # Import mappings of columns
     col_mapper = pd.read_csv(os.path.join(METADATA_PATH, 'columns.csv'))
@@ -19,7 +41,8 @@ if __name__ == '__main__':
     variable_descriptions = list(col_mapper['VariableRename'])
 
     # Read dataset
-    df = pd.read_csv(os.path.join(DATASET_PATH, 'psam_pusa.csv'))
+    print("Reading dataset")
+    df = pd.read_csv(os.path.join(DATASET_PATH, DATASET_NAME))
     #df = pd.read_csv(os.path.join(DATASET_PATH, 'test.csv'))
 
     # Extract columns and rename
