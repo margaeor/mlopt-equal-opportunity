23523197f5025b196d85041f0d8ec8c454d06e41
periclespet <periclespetrides@gmail.com>
2021-11-14 00:00:05 -0500

    ADD mapping on preprocessing

--- preprocess_data.py
+++ preprocess_data.py
@@ -6,6 +6,35 @@ from constants import *
 
 if __name__ == '__main__':
     input_file = OUTPUT_FILE
+
+    # PERFORM MAPPING OF CATEGORICAL DATA
+    out_data = input_file.fillna(0)
+    mappings = pd.read_csv(os.path.join(METADATA_PATH,'mapping.csv'))
+    variables_map = [x + '_map' for x in variables]
+    variable_aliases_map = [x + '_map' for x in variable_aliases]
+    mappings = mappings.rename(columns={k: v for k, v in zip(variables, variable_aliases)})
+    mappings = mappings.rename(columns={k: v for k, v in zip(variables_map, variable_aliases_map)})
+
+    output_mapped = pd.DataFrame()
+        
+    for var in variable_aliases:
+        if var in mappings.columns:
+            
+            # print(f"If : {var}")
+            mapFrom = list(mappings[var])
+            mapTo = list(mappings[var+'_map'])
+            initial_data = out_data[var]
+
+            mapped_data = list(map(lambda x : mapTo[list(mapFrom).index(x)], list(initial_data)))
+            output_mapped[var] = mapped_data
+            print(output_mapped[var])
+            print(mapped_data)
+        else:
+            output_mapped[var] = out_data[var]
+            # print(f"Else: {var}")
+
+    # output_mapped
+
     preprocessed_path = os.path.join(OUTPUT_PATH, 'preprocessed.csv')
 
     df_meta = pd.read_csv(METADATA_FILE)
